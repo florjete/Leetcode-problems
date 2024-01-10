350664e730bfcae14b27103d2a91041522f45c7d
Florjetë Kuka <59765295+florjete@users.noreply.github.com>
2024-01-10 14:52:16 +0100

    Update richestCustomer1672.cpp

--- Array/richestCustomer1672.cpp
+++ Array/richestCustomer1672.cpp
@@ -1,13 +1,18 @@
 #include <iostream>
 #include <vector>
+//You are given an m x n integer grid accounts where accounts[i][j] is the amount of money
+//the i​​​​​​​​​​​th​​​​ customer has in the j​​​​​​​​​​​th​​​​ bank. Return the wealth that the richest customer has.
+//A customer's wealth is the amount of money they have in all their bank accounts. 
+//The richest customer is the customer that has the maximum wealth.
 using namespace std;
 class Solution {
 public:
    int maximumWealth(vector<vector<int>>& accounts) {
     int maxWealth = 0;
-
+   //iteron mbi pasurine e cdo konsumatori
     for (const auto& customer : accounts) {
         int wealth = 0;
+       //iteron mbi cdo banke dhe mbledh pasurine e konsumatorit
         for (int money : customer) {
             wealth += money;
         }
